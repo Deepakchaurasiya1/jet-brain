30f233947dde0d747fc36acc52614dd13ce011dc
Deepak Chaurasiya <86309092+GaryMcKinnoDeepak@users.noreply.github.com>
2024-04-22 13:20:33 +0530

    Unlock the potential Section Done

--- assest/index.js
+++ assest/index.js
@@ -1,25 +1,35 @@
 // Video Section Start Here
 
-const video = document.getElementById('videoPlayer');
-const button = document.getElementById('toggleButton');
+const video = document.getElementById("videoPlayer");
+const button = document.getElementById("toggleButton");
 
-button.addEventListener('click', function () {
+button.addEventListener("click", function () {
   if (video.paused) {
     video.play();
-    button.textContent = 'Pause'; // Update button text
+    button.textContent = "Pause"; // Update button text
   } else {
     video.pause();
-    button.textContent = 'Play'; // Update button text
+    button.textContent = "Play"; // Update button text
   }
 });
 
 // Hide the button when the video is playing
-video.addEventListener('play', function () {
-  button.style.display = 'none';
+video.addEventListener("play", function () {
+  button.style.display = "none";
 });
 
 // Show the button when the video is paused
-video.addEventListener('pause', function () {
-  button.style.display = 'block';
+video.addEventListener("pause", function () {
+  button.style.display = "block";
 });
 // Video Section End Here
+
+// Spot Light Section Start Here
+
+const updateCursor = ({ x, y }) => {
+  document.documentElement.style.setProperty("--x", x);
+  document.documentElement.style.setProperty("--y", y);
+};
+document.body.addEventListener("pointermove", updateCursor);
+
+// Spot Light Section End Here
